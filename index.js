2dd89a6e2de14df301490ef8c110bf225ade40f9
adityagiri3600 <adityagiri3600@gmail.com>
2023-09-12 17:12:05 +0530

    fixed F=0 and F=1 cases

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ for (let _ = 0; _ < 3; _++) {
 
       let intersection = [...new Set(dict1[key1].filter(num => dict2[key2].includes(num)))];
       if (intersection.length > 0) {
-        let new_key = key1 + '.' + key2;
+        let new_key = key1 + key2;
         newcl.push({ [new_key]: intersection });
       }
     }
@@ -43,123 +43,145 @@ for (let _ = 0; _ < 3; _++) {
   classified_groups.push(newcl);
 }
 
-cubicTerms = [{"A'.C'.D'":[0,1]},{"A'.C'.D":[4,5]},{"A'.C.D":[12,13]},{"A'.C.D'":[8,9]},
-              {"A.C'.D'":[2,3]},{"A.C'.D":[6,7]},{"A.C.D":[14,15]},{"A.C.D'":[10,11]},
-              {"B'.C'.D'":[0,2]},{"B'.C'.D":[8,10]},{"B'.C.D":[12,14]},{"B'.C.D'":[4,6]},
-              {"B.C'.D'":[1,3]},{"B.C'.D":[9,11]},{"B.C.D":[13,15]},{"B.C.D'":[5,7]},
-              {"A'.B'.D'":[0,8]},{"A'.B'.D":[4,12]},{"A'.B.D":[5,13]},{"A'.B.D'":[1,9]},
-              {"A.B'.D'":[2,10]},{"A.B'.D":[6,14]},{"A.B.D":[7,15]},{"A.B.D'":[3,11]},
-              {"A'.B'.C'":[0,4]},{"A'.B'.C":[12,8]},{"A'.B.C":[13,9]},{"A'.B.C'":[1,5]},
-              {"A.B'.C'":[2,6]},{"A.B'.C":[14,10]},{"A.B.C":[11,15]},{"A.B.C'":[3,7]}]
+cubicTerms = [{ "A'C'D'": [0, 1] }, { "A'C'D": [4, 5] }, { "A'CD": [12, 13] }, { "A'CD'": [8, 9] },
+{ "AC'D'": [2, 3] }, { "AC'D": [6, 7] }, { "ACD": [14, 15] }, { "ACD'": [10, 11] },
+{ "B'C'D'": [0, 2] }, { "B'C'D": [8, 10] }, { "B'CD": [12, 14] }, { "B'CD'": [4, 6] },
+{ "BC'D'": [1, 3] }, { "BC'D": [9, 11] }, { "BCD": [13, 15] }, { "BCD'": [5, 7] },
+{ "A'B'D'": [0, 8] }, { "A'B'D": [4, 12] }, { "A'BD": [5, 13] }, { "A'BD'": [1, 9] },
+{ "AB'D'": [2, 10] }, { "AB'D": [6, 14] }, { "ABD": [7, 15] }, { "ABD'": [3, 11] },
+{ "A'B'C'": [0, 4] }, { "A'B'C": [12, 8] }, { "A'BC": [13, 9] }, { "A'BC'": [1, 5] },
+{ "AB'C'": [2, 6] }, { "AB'C": [14, 10] }, { "ABC": [11, 15] }, { "ABC'": [3, 7] }]
 classified_groups[3] = classified_groups[2]
 classified_groups[2] = cubicTerms
 for (let i = 0; i < 4; i++) {
   console.log(classified_groups[i][0]);
 }
 
-function condition(value,minterms,deleted_values) {
-  
-  
+function condition(value, minterms, deleted_values) {
+
+
   part1 = false
-  for(let i=0;i<value.length;i++) {
-    if([...minterms].includes(value[i])) {
+  for (let i = 0; i < value.length; i++) {
+    if ([...minterms].includes(value[i])) {
       part1 = true;
       break;
     }
   }
   part2 = true
-  for(let i=0;i<value.length;i++) {
-    if(!([...deleted_values].includes(value[i])) && !([...minterms].includes(value[i]))) {
+  for (let i = 0; i < value.length; i++) {
+    if (!([...deleted_values].includes(value[i])) && !([...minterms].includes(value[i]))) {
       part2 = false;
       break;
     }
   }
-  return part2&&part1
-  
+  return part2 && part1
+
 }
-let minterms = new Set([]);
 
 function solveKmap(minterms) {
-  
+  if(minterms.size === 16){
+    document.getElementById('groups').innerHTML = "Groups: 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"
+    document.getElementById('expression').innerHTML = "F = 1"
+    return
+  }
+  if(minterms.size === 0){
+    document.getElementById('groups').innerHTML = "Groups: "
+    document.getElementById('expression').innerHTML = "F = 0"
+    return
+  }
   let deleted_values = new Set();
   let deleted_values2 = new Set();
-  
+
   mintermsClone = new Set([...minterms])
 
-  expression = ""
   groups = []
-  
+
   for (let cg of classified_groups) {
     validGroups = []
     for (let group of cg) {
       let groupExpression = Object.keys(group)[0];
       let groupValues = group[groupExpression];
-      if (condition(groupValues,minterms,deleted_values)) {
-        validGroups.push([group,[...groupValues].filter(num => minterms.has(num)).length]);
+      if (condition(groupValues, minterms, deleted_values)) {
+        validGroups.push([group, [...groupValues].filter(num => minterms.has(num)).length]);
         groupValues.forEach(num => deleted_values.add(num));
         minterms = new Set([...minterms].filter(num => !deleted_values.has(num)));
       }
     }
     validGroups.sort((a, b) => a[1] - b[1]);
-    for(let i=0;i<validGroups.length;i++) {
+    for (let i = 0; i < validGroups.length; i++) {
       let group = validGroups[i][0];
       let groupExpression = Object.keys(group)[0];
       let groupValues = group[groupExpression];
-      if(condition(groupValues,mintermsClone,deleted_values)) {
+      if (condition(groupValues, mintermsClone, deleted_values)) {
         groupValues.forEach(num => deleted_values2.add(num));
         mintermsClone = new Set([...mintermsClone].filter(num => !deleted_values2.has(num)));
-        groups.push({value: groupValues,key: groupExpression})
+        groups.push({ value: groupValues, key: groupExpression })
       }
-      
+
     }
-    
+
   }
-  for(let i=0;i<groups.length;i++) {
+  for (let i = 0; i < groups.length; i++) {
     group = groups[i];
     groupNumbers = group.value;
     const usedNumbers = new Set();
-    for(const group of groups){
-      if(groupNumbers !== group.value){
-        for(const number of group.value){
+    for (const group of groups) {
+      if (groupNumbers !== group.value) {
+        for (const number of group.value) {
           usedNumbers.add(number);
         }
       }
     }
     count = 0;
-    for(const number of groupNumbers){
-      if(usedNumbers.has(number)){
+    for (const number of groupNumbers) {
+      if (usedNumbers.has(number)) {
         count++;
       }
     }
-    if(count === groupNumbers.length){
-      groups.splice(i,1);
+    if (count === groupNumbers.length) {
+      groups.splice(i, 1);
     }
   }
 
-  groups.sort((a,b) => a.value.length - b.value.length)
-   
-  document.getElementById('groups').innerHTML = "Groups: "
-  for(let i=0;i<groups.length;i++) {
-    let groupValue = groups[i].value
-    let groupExpression = groups[i].key
-    expression += groupExpression + " + "
-    document.getElementById('groups').innerHTML += groupValue.toString() + "&nbsp&nbsp&nbsp&nbsp"
-  }
-  document.getElementById('expression').innerHTML = "F = " + expression.slice(0,expression.length-2)
+  groups.sort((a, b) => a.value.length - b.value.length)
+
+  
+
+  setGroupHTML(groups)
+  setExpressionHTML(groups)
 }
+
+
+
+let minterms = new Set([]);
 solveKmap(minterms);
 
-kmorderList = [0,1,3,2,4,5,7,6,12,13,15,14,8,9,11,10]
 function kmorder(i) {
+  kmorderList = [0, 1, 3, 2, 4, 5, 7, 6, 12, 13, 15, 14, 8, 9, 11, 10]
   return kmorderList[i]
 }
+function setGroupHTML(groups) {
+  document.getElementById('groups').innerHTML = "Groups: "
+  for (let i = 0; i < groups.length; i++) {
+    let groupValue = groups[i].value
+    document.getElementById('groups').innerHTML += groupValue.toString() + "&nbsp&nbsp&nbsp&nbsp"
+  }
+}
+function setExpressionHTML(groups) {
+  let expression = ""
+  for(let i=0;i<groups.length;i++){
+    expression += groups[i].key + " + "
+  }
+  document.getElementById('expression').innerHTML = "F = " + expression.slice(0, expression.length - 2);
+}
+
 
 cells = document.getElementsByClassName('cell')
-for(let i=0;i<cells.length;i++) {
+for (let i = 0; i < cells.length; i++) {
   cells[i].innerHTML = `<span>${kmorder(i)}</span>`
-  cells[i].addEventListener('click',function() {
+  cells[i].addEventListener('click', function () {
 
-    if(minterms.has(kmorder(i))) {
+    if (minterms.has(kmorder(i))) {
       minterms.delete(kmorder(i))
       this.style.backgroundColor = "grey"
     } else {
